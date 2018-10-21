032427a701afef2130c8147a8de8a93914a4d899
xushu <xsxs1165208223@163.com>
2018-10-21 23:08:40 +0800

    更改了项目文件名

--- Odin/InitProgram.cs
+++ Odin/InitProgram.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace Odin
 {
-    static class Program
+    static class InitProgram
     {
         /// <summary>
         /// 应用程序的主入口点。
@@ -16,7 +16,7 @@ namespace Odin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
